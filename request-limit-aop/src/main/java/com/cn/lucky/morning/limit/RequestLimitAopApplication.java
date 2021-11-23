9d007107cedfe36b0ebfbcb3d0fe77b0a2f7fc6c
wangchen <wangc@youxuepai.com>
2021-11-23 16:08:33 +0800

    初始化

---
+++ request-limit-aop/src/main/java/com/cn/lucky/morning/limit/RequestLimitAopApplication.java
@@ -0,0 +1,16 @@
+package com.cn.lucky.morning.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lucky_morning
+ */
+@SpringBootApplication
+public class RequestLimitAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestLimitAopApplication.class, args);
+    }
+
+}
---
+++ request-limit-aop/src/test/java/com/cn/lucky/morning/limit/RequestLimitAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.lucky.morning.limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestLimitAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
